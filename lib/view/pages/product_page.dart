cbbd9dffc0f11ad38acc83ec64dfbb32e2ce5f1f
epashiq <ashiqashi4715@gmail.com>
2024-05-13 22:24:10 +0530

    create gridview builder

--- lib/view/pages/product_page.dart
+++ lib/view/pages/product_page.dart
@@ -5,6 +5,68 @@ class ProductPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.grey,
+        title: const Text('products'),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 20,
+            ),
+            GridView.builder(
+              physics: const ClampingScrollPhysics(),
+              shrinkWrap: true,
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 5,
+                  mainAxisSpacing: 5,
+                  mainAxisExtent: 200),
+              itemBuilder: (context, index) => Card(
+                elevation: 5,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Container(
+                        width: 220,
+                        height: 108,
+                        decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(8),
+                            image: const DecorationImage(
+                                image: NetworkImage(''), fit: BoxFit.cover)),
+                      ),
+                      const SizedBox(
+                        height: 8,
+                      ),
+                      const Text(
+                        '',
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                      const Text(
+                        '',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, fontSize: 12),
+                      ),
+                      const Text(
+                        '',
+                        style: TextStyle(
+                            fontWeight: FontWeight.w500, color: Colors.green),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              itemCount: 8,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
