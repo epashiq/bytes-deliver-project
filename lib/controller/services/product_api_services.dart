48e8e7cb2780edf2216f6d3140d055b87ef98e6f
epashiq <ashiqashi4715@gmail.com>
2024-05-14 11:17:43 +0530

    created a service class

--- lib/controller/services/product_api_services.dart
+++ lib/controller/services/product_api_services.dart
@@ -1,3 +1,27 @@
-class ProductServices{
-  
+import 'dart:developer';
+
+import 'package:bytes_deliver_project/core/utils/api_utils.dart';
+import 'package:bytes_deliver_project/model/product_model.dart';
+import 'package:dio/dio.dart';
+
+class ProductServices {
+  static Future<List<ProductModel>?> getProducts(int page) async {
+    final dio = Dio();
+    try {
+      dio.options.headers['appid'] = '2IPbyrCUM7s5JZhnB9fxFAD6';
+      FormData data = FormData.fromMap({'page': page});
+      final Response resp = await dio.post(ApiUtils.apiUrl, data: data);
+      if (resp.statusCode == 200) {
+        log(resp.statusCode.toString());
+        final producList = <ProductModel>[];
+        for (final result in resp.data['list']) {
+          producList.add(ProductModel.fromJson(result));
+        }
+        return producList;
+      }
+    } catch (e) {
+      log(e.toString());
+    }
+    return null;
+  }
 }
